6da264fc1cee9e79051a573a46f70a20707805d7
israeljulio <israel.jhonatas@sesisenaibetim.com.br>
2022-06-16 12:38:21 -0300

    adicionando propriedades iniciais

--- include/Livro.h
+++ include/Livro.h
@@ -1,13 +1,22 @@
 #ifndef Livro_H
 #define Livro_H
+#include<iostream>
+#include<string>
 
+#include "EGenero.h"
 using namespace std;
 
 class Livro{
     private:
-
+        int _id;
+        string _nome;
+        EGenero _genero;
     public:
-
+        Livro(int id, string nome, EGenero genero){
+            _id = id;
+            _nome = nome;
+            _genero = genero;
+        }
 };
 
 #endif
