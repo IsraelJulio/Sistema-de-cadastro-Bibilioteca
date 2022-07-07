c58330348ab3ed51be64b0cc6d78563e65a3ba08
israeljulio <israel.jhonatas@sesisenaibetim.com.br>
2022-07-07 19:36:44 -0300

    troca de .h por .hpp

---
+++ include/Administrador.hpp
@@ -0,0 +1,27 @@
+#ifndef Administrador_H
+#define Administrador_H
+#include<vector>
+#include"User.hpp"
+#include"Livro.hpp"
+#include "Usuario.hpp"
+using namespace std;
+
+class Administrador: public User{
+    private:
+        vector<User*> _usuariosAtivos;
+        vector<Livro*> _livrosBloqueados;
+        vector<Livro*> _livrosDevolvidos;
+
+    public:
+        Administrador(string nome, string matricula, EPerfil perfil);
+        virtual void Imprime() override;
+        vector<Livro*>ListarLivrosBloqueados(vector<Livro*> livros);
+        vector<User*>ListarUsuariosAtivos(vector<User*> usuarios);
+        bool SetUsuario(User* Usuario);
+        bool SetLivro(int id, string nome, EGenero genero);
+        bool EditarUsuario(string matricula);
+        bool AtualizarListaDeUsuarios(vector<User *> list);
+        virtual string GetDados() override;
+};
+
+#endif
